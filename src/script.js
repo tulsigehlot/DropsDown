78d2a081aad7155b8a60c2431add0bfe9c6e7210
Tulsi Gehlot <tulsigehlot6@gmail.com>
2022-07-05 15:44:12 +0530

    initiated js and updated html

--- src/script.js
+++ src/script.js
@@ -1,6 +1,29 @@
 'use strict'
 
-const input = document.querySelector('.search').value;
+const input = document.querySelector('.search');
 const searchIcon = document.querySelector('.icon');
 const currLocation = document.querySelector('.current--location');
 const clearBtn = document.querySelector('.clear');
+
+
+////////////////////////////clear inputs and button////////////////////////////////////////
+
+clearBtn.addEventListener('click',function(){
+    input.value = '';
+});
+
+////////////////////////////get device location////////////////////////////////////////
+
+let lat,lng;
+function getGeolocation (position){
+    navigator.geolocation.getCurrentPosition((position) => {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        console.log(lat,lng);
+    },() => {
+        alert("User denied to access the location ");
+    });
+
+};
+
+currLocation.addEventListener('click',getGeolocation);
