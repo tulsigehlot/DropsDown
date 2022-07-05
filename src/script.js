4085c89bb75a21f4dec675847a774594679c5202
Tulsi Gehlot <tulsigehlot6@gmail.com>
2022-07-05 16:04:03 +0530

    initiated js and updated html and css

--- src/script.js
+++ src/script.js
@@ -4,12 +4,15 @@ const input = document.querySelector('.search');
 const searchIcon = document.querySelector('.icon');
 const currLocation = document.querySelector('.current--location');
 const clearBtn = document.querySelector('.clear');
-
+const quoteContainer = document.querySelector('.container');
+const cardsContainer = document.querySelector('.card');
 
 ////////////////////////////clear inputs and button////////////////////////////////////////
 
 clearBtn.addEventListener('click',function(){
     input.value = '';
+    quoteContainer.classList.remove('hidden');
+    cardsContainer.classList.add('hidden');
 });
 
 ////////////////////////////get device location////////////////////////////////////////
