4f5bccd833ebb5b2a95e8e3199f4df4ec6a60b31
Tulsi Gehlot <tulsigehlot6@gmail.com>
2022-07-05 15:14:47 +0530

    initiated js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const input = document.querySelector('.search').value;
+const searchIcon = document.querySelector('.icon');
+const currLocation = document.querySelector('.current--location');
+const clearBtn = document.querySelector('.clear');
